a1563bf6dabb623a67f90396a08d8416da7fec1d
TonyVirgin <60446947+TonyVirgin@users.noreply.github.com>
2024-11-01 21:47:42 -0500

    Project setup

---
+++ Cemetery/src/pages/record/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IAddRecordProps {
+}
+
+const AddRecord: React.FunctionComponent<IAddRecordProps> = (props) => {
+  return <div>AddRecord</div>;
+};
+
+export default AddRecord;
